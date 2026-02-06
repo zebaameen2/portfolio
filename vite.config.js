e54c9eabd1520bf624d3d2677aa217b3d4d4c8b2
zebaameen2 <tayyabameen276@gmail.com>
2026-02-06 23:37:56 +0530

    Remove tailwind vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react() ,tailwindcss()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() ,tailwindcss()],
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// }) ...
 
