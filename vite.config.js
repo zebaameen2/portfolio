660ad1aef26a2651ee99243b80a1121ca4686d45
zebaameen2 <tayyabameen276@gmail.com>
2026-02-07 18:36:56 +0530

    aaded again

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-// }) ...
 
