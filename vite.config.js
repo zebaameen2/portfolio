031f06932d7197bada1e280638ffce973ac04e88
zebaameen2 <tayyabameen276@gmail.com>
2026-02-07 18:59:05 +0530

    Fix Tailwind v4 + PostCSS config for Netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
 })
 
